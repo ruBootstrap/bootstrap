66693b09a8cc1d570ec7d34d4497372c040a97fe
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2020-10-02 15:37:24 +0300

    Перевл в скрипте кнопки

--- site/assets/js/application.js
+++ site/assets/js/application.js
@@ -75,7 +75,7 @@
       var modalTitle = exampleModal.querySelector('.modal-title')
       var modalBodyInput = exampleModal.querySelector('.modal-body input')
 
-      modalTitle.textContent = 'New message to ' + recipient
+      modalTitle.textContent = 'Новое сообщение для ' + recipient
       modalBodyInput.value = recipient
     })
   }
@@ -92,7 +92,7 @@
   }
 
   // Insert copy to clipboard button before .highlight
-  var btnHtml = '<div class="bd-clipboard"><button type="button" class="btn-clipboard" title="Copy to clipboard">Copy</button></div>'
+  var btnHtml = '<div class="bd-clipboard"><button type="button" class="btn-clipboard" title="Скопировать в буфер обмена">Копировать</button></div>'
   document.querySelectorAll('figure.highlight, div.highlight')
     .forEach(function (element) {
       element.insertAdjacentHTML('beforebegin', btnHtml)
@@ -119,22 +119,22 @@
   clipboard.on('success', function (e) {
     var tooltipBtn = bootstrap.Tooltip.getInstance(e.trigger)
 
-    e.trigger.setAttribute('data-original-title', 'Copied!')
+    e.trigger.setAttribute('data-original-title', 'Скопировано!')
     tooltipBtn.show()
 
-    e.trigger.setAttribute('data-original-title', 'Copy to clipboard')
+    e.trigger.setAttribute('data-original-title', 'Скопировать в буфер обмена')
     e.clearSelection()
   })
 
   clipboard.on('error', function (e) {
     var modifierKey = /mac/i.test(navigator.userAgent) ? '\u2318' : 'Ctrl-'
-    var fallbackMsg = 'Press ' + modifierKey + 'C to copy'
+    var fallbackMsg = 'Нажмите ' + modifierKey + 'C для копирования'
     var tooltipBtn = bootstrap.Tooltip.getInstance(e.trigger)
 
     e.trigger.setAttribute('data-original-title', fallbackMsg)
     tooltipBtn.show()
 
-    e.trigger.setAttribute('data-original-title', 'Copy to clipboard')
+    e.trigger.setAttribute('data-original-title', 'Скопировать в буфер обмена')
   })
 
   anchors.options = {
